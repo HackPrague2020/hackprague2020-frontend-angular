398a341c828fe36742cca84c72b8ccd80efa45a5
Tomas Ye <tomaye@amazon.com>
2020-10-31 14:15:27 +0100

    [HELLO API CALL]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { IncomeStatementComponent } from './components/income-statement/income-statement.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IncomeStatementComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/income-statement/income-statement.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {StockDataService} from '../../services/stock-data/stock-data.service';
+
+@Component({
+  selector: 'app-income-statement',
+  templateUrl: './income-statement.component.html',
+  styleUrls: ['./income-statement.component.css']
+})
+export class IncomeStatementComponent implements OnInit {
+  sample_data: any;
+
+  constructor(private stockDataService: StockDataService) { }
+
+  ngOnInit(): void {
+    this.stockDataService.getData().subscribe(data => {
+      console.log(data);
+      this.sample_data = JSON.stringify(data);
+    })
+  }
+
+}
