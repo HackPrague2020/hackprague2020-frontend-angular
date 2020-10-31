3caca3b6688ed57b762ef5f570fd5b34c2dac2dc
Tomas Ye <tomaye@amazon.com>
2020-10-31 19:01:45 +0100

    [RADAR CHART] Hello world radar chart works

--- src/app/components/performance-radar-chart/performance-radar-chart.component.ts
+++ src/app/components/performance-radar-chart/performance-radar-chart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ChartDataSets, ChartType, RadialChartOptions} from 'chart.js';
+import {Label} from 'ng2-charts';
+import {StockDataService} from '../../services/stock-data/stock-data.service';
 
 @Component({
   selector: 'app-performance-radar-chart',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerformanceRadarChartComponent implements OnInit {
 
-  constructor() { }
+  // Radar
+  public radarChartOptions: RadialChartOptions = {
+    responsive: true,
+  };
+  public radarChartLabels: Label[] = ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'];
+
+  public radarChartData: ChartDataSets[] = [
+    { data: [65, 59, 90, 81, 56, 55, 40], label: 'Series A' },
+  ];
+  public radarChartType: ChartType = 'radar';
+
+  constructor(private stockDataService: StockDataService) { }
 
   ngOnInit(): void {
+
   }
 
+  // events
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
 }
+
